7498eadea4769e3cacd0e38f099494b962a054ad
Your Name <you@example.com>
2025-07-23 07:49:52 +0545

    Event_booking_system

--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import api_view
-from .models import Category
+from .models import *
 from .serializer import CategorySerializer
-# Create your views here.
+from rest_framework.serializers import ValidationError
+# Create your views here. 
 
 @api_view(['GET','POST'])
 def category_list(request):
@@ -20,36 +21,25 @@ def category_list(request):
             'message': 'Category created successfully',
         })
 
-@api_view(['GET','POST'])
-def table_list(request):
-    if request.method == "GET":
-        table = Table.objects.all()
-        serializer = TableSerializer(table, many=True)
-        return Response (serializer.data)
-    elif request.method == 'POST':
-        serializer = TableSerializer(data= request.data)
-        serializer.is_valid(raise_exception= True)
-        serializer.save()
-        return Response ({
-            "number": "2",
-            "size" : "9",
-            "available" : True,
-        })
-        
-@api_view(['PUT'])
-def table_update(request, id):
-    if request.method == "PUT":
-        table = Table.objects.get(id=id)
-        serializer = TableSerializer(table, data=request.data)
-        serializer.is_valid(raise_exception = True)
-        serializer.save()
-        return Response(serializer.data)
-    
-class SnippetSerializer()
 
-@api_view(['GET'])
+@api_view(['GET','DELETE'])
 def category_details(request,id):
+    category = Category.objects.get(id = id)
     if request.method == 'GET':
-        category = Category.objects.get(id = id)
         serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        total = OrderItem.objects.filter(food__category = category).count()
+        if total > 0:
+            raise ValidationError({
+                'error': 'You cannot delete this category because it has orders',
+            })
+        category.delete()
+        return Response({
+            'message': 'Category deleted successfully',
+        })
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(data = request.data)
+        serializer.is_vaild(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
